0fb0f0a4a67563da022f8843135e920381d2023a
JetLiTheQT <ekkachai.ittihrit2@gmail.com>
2021-05-31 21:41:12 -0700

    Wrote Test for multiple of 5

--- testfizzbuzz.py
+++ testfizzbuzz.py
@@ -4,3 +4,9 @@ import fizzbuzz
 class TestMultiples(unittest.TestCase):
     def testThree(self):
         self.assertEqual(fizzbuzz.fizzbuzz(3), "Fizz")
+        self.assertEqual(fizzbuzz.fizzbuzz(6), "Fizz")
+        self.assertEqual(fizzbuzz.fizzbuzz(9), "Fizz")
+    def testFive(self):
+        self.assertEqual(fizzbuzz.fizzbuzz(5), "Buzz")
+        self.assertEqual(fizzbuzz.fizzbuzz(10), "Buzz")
+        self.assertEqual(fizzbuzz.fizzbuzz(50), "Buzz")
